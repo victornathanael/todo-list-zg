7d49d71f213643c5bb2ac76ea85b44d0cd88d116
Victor Nathanael <victornathanael15@gmail.com>
2023-08-13 02:58:26 -0300

    feat: implement task menu display

---
+++ src/view/TaskMenu.java
@@ -0,0 +1,10 @@
+package view;
+
+public class TaskMenu {
+    public static void taskMenu() {
+        System.out.println();
+        System.out.println("Menu:\n1. Criar tarefa\n2. Listar tarefas\n3. Listar tarefas por categoria, prioridade ou status\n4. Setar o status da atividade\n5. Deletar tarefa\n6. Sair");
+        System.out.print("Escolha uma opção: ");
+
+    }
+}
