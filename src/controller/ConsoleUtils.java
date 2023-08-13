d130b6374c4e0894399464b8cae0ebfcc6a8dbb5
Victor Nathanael <victornathanael15@gmail.com>
2023-08-13 02:40:38 -0300

    feat: add console utility functions

---
+++ src/controller/ConsoleUtils.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class ConsoleUtils {
+    public static void clearConsole() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+}
